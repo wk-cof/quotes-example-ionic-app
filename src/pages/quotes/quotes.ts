5445a994de3f7999e998c71a00be7b1d86ee4d49
gorbachevma <gorbachevma@gmail.com>
2018-10-17 19:05:05 +0100

    Added quotes and set up for dev

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import text1 from '../../assets/txt/sample-text-1';
+import text2 from '../../assets/txt/sample-text-2';
+import text3 from '../../assets/txt/sample-text-3';
+import text4 from '../../assets/txt/sample-text-4';
+
 /**
  * Generated class for the QuotesPage page.
  *
@@ -14,8 +19,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'quotes.html',
 })
 export class QuotesPage {
+  public quotes;
+  public currentQuote = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.quotes = [];
+    this.quotes.push(text1);
+    this.quotes.push(text2);
+    this.quotes.push(text3);
+    this.quotes.push(text4);
   }
 
   ionViewDidLoad() {
